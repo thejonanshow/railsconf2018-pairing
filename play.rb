8dd53797895b17938a1a86f86a4792ddb5447962
Jonan Scheffler <jonanscheffler@gmail.com>
2018-04-18 16:29:31 -0400

    WHY RUBY PEOPLE, WHYYYYYYYY

--- play.rb
+++ play.rb
@@ -1,9 +1,36 @@
 require 'rspec'
 
+class Cell
+  attr_accessor :alive
+
+  def initialize
+    @alive = true
+  end
+
+  def kill
+    @alive = false
+  end
+
+  def dead?
+    !alive
+  end
+
+  def alive?
+    alive
+  end
+end
+
 # live < 2, die
 # live 2/3, live
 # live with > 3, die
 # dead with == 3, spawn
 
 RSpec.describe 'Conway' do
+  describe Cell do
+    subject { Cell.new }
+    
+    it "can be killed" do
+      expect{ subject.kill }.to change { subject.dead? }.from(false).to(true)
+    end
+  end
 end
