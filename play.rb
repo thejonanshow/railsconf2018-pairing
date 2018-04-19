94573acec3dd90b7b71bfc52d92414f543a1d9fa
Jonan Scheffler <jonanscheffler@gmail.com>
2018-04-19 11:01:12 -0400

    Thank you KERRI!!!

--- play.rb
+++ play.rb
@@ -5,5 +5,64 @@ require 'rspec'
 # live with > 3, die
 # dead with == 3, spawn
 
-RSpec.describe 'Conway' do
+class Oyster
+  def initialize(aquarium)
+    @aquarium = aquarium
+  end
+
+  def friends
+    @aquarium.who_are_my_friends(self)
+  end
 end
+
+class Aquarium
+  def oysters
+    []
+  end
+
+  def who_are_my_friends(oyster)
+    
+  end
+end
+
+RSpec.describe 'Conway' do
+  describe Oyster do
+    let(:oyster) { Oyster.new(Aquarium.new) }
+
+    it "is an Oyster" do
+      expect(oyster).to be_a Oyster
+    end
+
+    context "friends" do
+      it "starts without friends" do
+        expect(oyster.friends).to be_empty
+      end
+
+      it "reports on its friends" do
+        expect(oyster.friends).not_to be_empty
+        expect(oyster.friends.count).to be_greater_than 1
+      end
+    end
+  end
+
+  describe Aquarium do
+    let(:aquarium) { Aquarium.new }
+
+    it "has oysters" do
+      expect(aquarium.oysters).to respond_to(:size)
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
